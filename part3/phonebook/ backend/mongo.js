62be89353253204247c6bc8921e446e464ad2e44
Lasse Lautanala <lasselautanala@gmail.com>
2024-04-18 12:10:08 +0300

    Full Stack Open Part3

---
+++ part3/phonebook/ backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+const password = process.argv[2]
+
+url = `mongodb+srv://lasselautanala:${password}@fullstackdatabase.j1z9b3g.mongodb.net/phonebookapp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 5) {
+  const person = new Person({name: process.argv[3], number: process.argv[4]})
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
